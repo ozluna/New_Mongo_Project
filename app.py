54a05ae60307fa93458a9b44a2ef275c13a9bef9
Ozge <ozonel883@hotmail.com>
2020-08-03 19:16:41 +0000

    new edit template added, binded withe the app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
@@ -9,6 +9,7 @@ app.config["Mongo_DBNAME"] = 'task_manager'
 app.config['MONGO_URI'] = 'mongodb+srv://root:1amS3cur3@cluster0.hrobk.mongodb.net/task_manager?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
+
 
 @app.route("/")
 @app.route('/get_tasks')
@@ -25,6 +26,34 @@ def add_task():
 def insert_task():
     tasks = mongo.db.tasks
     tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
+
+
+@app.route("/edit_task/<task_id>")
+def edit_task(task_id):
+    the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    all_categories = mongo.db.categories.find()
+    return render_template('edittask.html', task=the_task, categories=all_categories)
+
+
+@app.route('/update_task/<task_id>', methods=["POST"])
+def update_task(task_id):
+    tasks = mongo.db.tasks
+    tasks.update({'_id': ObjectId(task_id)},
+    {
+        'task_name': request.form.get('task_name'),
+        'category_name': request.form.get('category_name'),
+        'task_description': request.form.get('task_description'),
+        'due_date': request.form.get('task.due_date'),
+        'is_urgent': request.form.get('task.is_urgent')
+
+    })
+    return redirect(url_for('get_tasks'))
+
+
+@app.route('/delete_task/<task_id>')
+def delete_task(task_id):
+    mongo.db.tasks.remove({'_id': ObjectId(task_id)})
     return redirect(url_for('get_tasks'))
 
 
